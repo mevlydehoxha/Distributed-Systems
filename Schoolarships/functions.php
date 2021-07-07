bb47863d05879dd04ef8d2b0929f23de9a6ce6fa
EtnikaHalili <57642537+EtnikaHalili@users.noreply.github.com>
2021-07-08 01:17:48 +0200

    Create functions.php

---
+++ Schoolarships/functions.php
@@ -0,0 +1,43 @@
+<?php
+function pdo_connect_mysql() {
+    $servername = "localhost:3325";
+    $username = "root";
+    $password = "";
+    $dbname="students";
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn;
+      echo "Connected successfully";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+}
+function template_header($title) {
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+    <nav class="navtop">
+    	<div>
+    		<h1>Website Title</h1>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+    		<a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
+    	</div>
+    </nav>
+EOT;
+}
+function template_footer() {
+echo <<<EOT
+    </body>
+</html>
+EOT;
+}
+?>
